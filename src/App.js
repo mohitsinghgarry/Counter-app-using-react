57b5e27fa1468289d126d119d893fc0addf3091a
mohitsinghgarry <mohitsinghx4@gamil.com>
2023-11-23 14:25:10 +0530

    counter app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
+import React from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const [counter , newState] = useState(0);
+  let incrementHandler=()=>{
+    // console.log(counter+1);
+    newState(counter+1);
+
+  }
+  let resetHandler=()=>{
+    newState(0);
+  }
+ let decrementHandler=()=>{
+    // console.log(counter-1);
+    newState(counter-1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div className="block">
+        <div className="counter">
+            <h3 className='co'>Counter</h3>
+            <h1>{counter}</h1>
+        </div>
+        <div>
+            <button onClick={incrementHandler} id="btn" type="button" className="btn btn-success">Increase</button>
+            <button onClick={resetHandler}  type="button" className="btn btn-warning">Reset</button>
+            <button onClick={decrementHandler}  id="btn1" type="button" className="btn btn-danger">Decrease</button>
+        </div>
     </div>
+</div>
   );
 }
 
